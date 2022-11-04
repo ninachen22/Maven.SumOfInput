3147e6b8d538863528d7792a078225c1ef34811d
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-04 16:32:12 -0400

    made changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,36 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        System.out.println("Please input a number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //invoking the method/calling the method
+        int sum = sumOfNumbers(n);
+        System.out.println("The sum is: " + sum);
 
     }
-    
-    int sumOfNumbers(int n) {
-        return 0;
+
+    static int sumOfNumbers(int n) { //method that takes int n from scanner above
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+//            System.out.println(sum);
+        }
+        return sum;
     }
-    
+
+
     // for Extra Credit
     int gaussianSumOfNumbers(int n) {
         return 0;
     }
-   
+
     // for Extra extra credit - compare the methods and show which one is faster
-    
+
 }
